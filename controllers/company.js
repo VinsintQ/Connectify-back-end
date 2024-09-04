2a2816c225b827706583748c229a71a5a3293b3a
Mujtaba_Sultan <b.mojtabaali@gmail.com>
2024-09-04 14:51:17 +0300

    did the routed for jobs in companies , just need to test them through postamn

--- controllers/company.js
+++ controllers/company.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const Company = require("../models/company");
+const Job = require("../models/job");
 const verifyToken = require("../middleware/verify-token");
 
+//company routers ------------------------
 router.get("/", async (req, res) => {
   try {
     const companies = await Company.find();
@@ -22,6 +24,7 @@ router.get("/:companyId", async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+
 router.post("/", async (req, res) => {
   try {
     req.body.owner = req.user._id;
@@ -37,8 +40,10 @@ router.put("/:companyId", async (req, res) => {
     if (!company) {
       res.status(404).json({ error: "company not found" });
     } else {
-
-      const updatedCompany=await Company.findByIdAndUpdate(req.params.companyId, req.body);
+      const updatedCompany = await Company.findByIdAndUpdate(
+        req.params.companyId,
+        req.body
+      );
       res.status(200).json(updatedCompany);
     }
   } catch (error) {
@@ -46,10 +51,72 @@ router.put("/:companyId", async (req, res) => {
   }
 });
 router.delete("/:companyId", async (req, res) => {
+  deletedCompany = await Company.findByIdAndDelete(req.params.companyId);
+  res.status(200).json({ message: `company deleted` });
+});
 
-deletedCompany=await Company.findByIdAndDelete(req.params.companyId);
-res.status(200).json({message:`${deletedCompany} deleted`});
+//jobs router ----------------------------
 
+router.get("/:companyId/jobs", async (req, res) => {
+  try {
+    const jobs = await Job.find({ company: req.params.companyId });
+    res.status(200).json(jobs);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/:companyId/jobs", async (req, res) => {
+  try {
+    const company = await Company.findById(req.params.companyId);
+    req.body.company = company._id;
+    const job = await Job.create(req.body);
+    res.status(200).json(job);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get("/:companyId/jobs/:jobId ", async (req, res) => {
+  try {
+    const job = await Job.find({
+      company: req.params.companyId,
+      _id: req.params.jobId,
+    });
+    res.status(200).json(job);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.put("/:companyId/jobs/:jobId ", async (req, res) => {
+  try {
+    const company = await Company.findById(req.params.companyId);
+
+    if (company.owner !== req.user._id) {
+      res.status(401).json({ error: "Unauthorized" });
+    } else {
+      const job = await Job.findByIdAndUpdate(req.params.jobId, req.body);
+      res.status(200).json(job);
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.delete("/:companyId/jobs/:jobId ", async (req, res) => {
+  try {
+    const company = await Company.findById(req.params.companyId);
+
+    if (company.owner !== req.user._id) {
+      res.status(401).json({ error: "Unauthorized" });
+    } else {
+      const job = await Job.findByIdAndDelete(req.params.jobId);
+      res.status(200).json({ message: "job deleted successfully" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports = router;
--- models/job.js
+++ models/job.js
@@ -3,24 +3,25 @@ const mongoose = require("mongoose");
 const musthaveSchema = mongoose.Schema({
   text: { required: true, type: String },
 });
+
 const nicetohaveSchema = mongoose.Schema({
   text: { required: true, type: String },
 });
+
 const jobSchema = mongoose.Schema({
   jobtitle: { required: true, type: String },
-  workplace: { required: true, enum: ["onsite", "hybrid", "remote"] },
   location: { required: true, type: String },
   company: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Comapany",
   },
+  workplace: { enum: ["onsite", "hybrid", "remote"] },
   jobtype: {
-    required: true,
     enum: ["fulltime", "parttime", "internhip", "temporary"],
   },
   overview: String,
-  musthave: { required: true, type: [musthaveSchema] },
-  nicetohave: { required: true, type: [nicetohaveSchema] },
+  musthave: [musthaveSchema],
+  nicetohave: [nicetohaveSchema],
 });
 
 module.exports = mongoose.model("Job", jobSchema);
