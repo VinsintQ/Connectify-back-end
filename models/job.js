63b00e6b724ca19d11da2c5cb8f640951560e871
Mueen <hejiy2002@gmail.com>
2024-09-04 10:45:02 +0300

    adding must have and nice to have schema to gob schema

--- models/job.js
+++ models/job.js
@@ -1,5 +1,11 @@
 const mongoose = require("mongoose");
 
+const musthaveSchema = mongoose.Schema({
+  text: { required: true, type: String },
+});
+const nicetohaveSchema = mongoose.Schema({
+  text: { required: true, type: String },
+});
 const jobSchema = mongoose.Schema({
   jobtitle: { required: true, type: String },
   workplace: { required: true, enum: ["onsite", "hybrid", "remote"] },
@@ -12,6 +18,9 @@ const jobSchema = mongoose.Schema({
     required: true,
     enum: ["fulltime", "parttime", "internhip", "temporary"],
   },
+  overview: String,
+  musthave: { required: true, type: [musthaveSchema] },
+  nicetohave: { required: true, type: [nicetohaveSchema] },
 });
 
 module.exports = mongoose.model("Job", jobSchema);
