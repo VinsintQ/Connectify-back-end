5d232a1c22c2c40fcb56bd933f88e8d73b574a79
ali-alaalem <Meemo2002a@gmail.com>
2024-09-05 10:32:01 +0300

    Adding the projecct controller routes (create ,delete ,update,get)

--- models/project.js
+++ models/project.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
-const projectSchema = mongoose.Schema({
-  Description: { required: true, type: String },
-  skills: [proskillSchema],
-});
-
 const proskillSchema = mongoose.Schema({
   skill: { required: true, type: String },
 });
+
+const projectSchema = mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  description: { required: true, type: String },
+  skills: [proskillSchema],
+});
+
 module.exports = mongoose.model("project", projectSchema);
