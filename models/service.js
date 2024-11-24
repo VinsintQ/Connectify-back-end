e182416db961200c38fc49a6d8aa7c819c50e340
Mueen <hejiy2002@gmail.com>
2024-11-24 20:58:35 +0300

    modify service model

--- models/service.js
+++ models/service.js
@@ -4,7 +4,6 @@ const serviceSchema = mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   serviceTitle: { required: true, type: String },
   description: { required: true, type: String },
-  category: { required: true, type: String },
   startingPrice: { required: true, type: Number },
 });
 
